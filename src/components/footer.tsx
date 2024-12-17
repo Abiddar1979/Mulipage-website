d080622ed16158e048413ab045a4a0ebe7bdee71
Abid Dar <164938778+Abiddar1979@users.noreply.github.com>
2024-12-18 00:50:04 +0500

    Add files via upload

---
+++ src/components/footer.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { FaOtter } from 'react-icons/fa'
+
+const footer = () => {
+  return (
+    <footer> &copy; 2024 RC Traxxes cars. All rights Reserved.</footer>
+  )
+}
+
+export default footer
